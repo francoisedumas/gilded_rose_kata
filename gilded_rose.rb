8b423ffb8c550d56c797104d2396208cdfd3e621
Francois Dumas <dumaslattaque.francois@gmail.com>
2022-11-17 22:36:05 +0100

    Feat - Remove the big tick function

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -6,49 +6,59 @@ class Item
   end
 
   def tick
-    if @name != 'Aged Brie' && @name != 'Backstage passes to a TAFKAL80ETC concert'
-      if @quality > 0
-        if @name != 'Sulfuras, Hand of Ragnaros'
-          @quality -= 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality += 1
-        if @name == 'Backstage passes to a TAFKAL80ETC concert'
-          if @sell_in < 11
-            if @quality < 50
-              @quality += 1
-            end
-          end
-          if @sell_in < 6
-            if @quality < 50
-              @quality += 1
-            end
-          end
-        end
-      end
+    case name
+    when 'normal'
+      return normal_tick
+    when 'Aged Brie'
+      return brie_tick
+    when 'Sulfuras, Hand of Ragnaros'
+      return sulfuras_tick
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      return backstage_tick
     end
-    if @name != 'Sulfuras, Hand of Ragnaros'
-      @sell_in -= 1
-    end
-    if @sell_in < 0
-      if @name != "Aged Brie"
-        if @name != 'Backstage passes to a TAFKAL80ETC concert'
-          if @quality > 0
-            if @name != 'Sulfuras, Hand of Ragnaros'
-              @quality -= 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality += 1
-        end
-      end
-    end
+  end
+
+  # initial method making test pass green again
+  # def normal_tick
+  #   if @quality != 0
+  #     if @sell_in > 0
+  #       @quality -= 1
+  #     elsif @sell_in <= 0
+  #       @quality -= 2
+  #     end
+  #   end
+  #   @sell_in -= 1
+  # end
+
+  # DRY method making test pass green
+  def normal_tick
+    @sell_in -= 1
+    return if @quality == 0
+
+    @quality -= 1
+    @quality -= 1 if @sell_in <= 0
+  end
+
+  def brie_tick
+    @sell_in -= 1
+    return @quality = 50 if @quality == 49
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @sell_in <= 0
+  end
+
+  def sulfuras_tick
+  end
+
+  def backstage_tick
+    @sell_in -= 1
+    return if @quality >= 50
+    return @quality = 0 if @sell_in < 0
+
+    @quality += 1
+    @quality += 1 if @sell_in < 10
+    @quality += 1 if @sell_in < 5
   end
 end
 
